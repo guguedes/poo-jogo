b597a07f3dddeb37afce554b836200c39589e7cc
Professor <professor@ifpr.edu.br>
2023-10-10 11:58:34 -0300

    vigu

---
+++ src/main/java/Guerreiro.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+class Guerreiro {
+    private String nome;
+    private double PontosVida;
+    private int PontosAtaque;
+    private int PontosDefesa;
+    private int Forca;
+    private int Velocidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPontosVida() {
+        return PontosVida;
+    }
+
+    public void setPontosVida(double PontosVida) {
+        this.PontosVida = PontosVida;
+    }
+
+    public int getPontosAtaque() {
+        return PontosAtaque;
+    }
+
+    public void setPontosAtaque(int PontosAtaque) {
+        this.PontosAtaque = PontosAtaque;
+    }
+
+    public int getPontosDefesa() {
+        return PontosDefesa;
+    }
+
+    public void setPontosDefesa(int PontosDefesa) {
+        this.PontosDefesa = PontosDefesa;
+    }
+
+    public int getForca() {
+        return Forca;
+    }
+
+    public void setForca(int Forca) {
+        this.Forca = Forca;
+    }
+
+    public int getVelocidade() {
+        return Velocidade;
+    }
+
+    public void setVelocidade(int Velocidade) {
+        this.Velocidade = Velocidade;
+    }
+}
